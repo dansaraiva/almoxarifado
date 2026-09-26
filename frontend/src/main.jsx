532d6aaed0597b29548ac3caf74463a6f6e4e8f9
KeltonFelix <raimundokeltonk@gmail.com>
2026-09-26 09:10:16 -0300

    chore: instala e configura React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import Home from './pages/Home/Home.jsx';
+import Sobre from './pages/Sobre/Sobre.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">Página Home</h1>
+      <p className="mt-4">Bem-vindo ao Almoxarifado!</p>
+    </div>
+  );
+}
+
+export default Home;
